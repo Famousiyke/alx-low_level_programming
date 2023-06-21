7994cd326098ebde3355748e6994d865404e3f65
famousiyke <rowlandiyke88@gmail.com>
2023-06-21 12:34:34 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * print_alphabet - print alphabet in lowercase followed by a new line
- * 
+ *
  * Return: Always 0 if successful
  */
 void print_alphabet(void)
-{	
+{
 	int i;
-	for (i = 97; i <=122; i++)
+	for (i = 97; i <= 122; i++)
+
 	{
 		_putchar(i);
 	}
