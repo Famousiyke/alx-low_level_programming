04c333c438ec305bc5463240ee53d2271a4cb0da
famousiyke <rowlandiyke88@gmail.com>
2023-07-03 05:07:30 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A c program that prints a line to the standard error
+ * followed by a new line
+ *
+ * Return: Always 1(succesful)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar,
+	2015-10-19\n", stdout);
+	return (1);
+}
