6e4b0e73e03e539064794ed35f98ce82712f6a8d
famousiyke <rowlandiyke88@gmail.com>
2023-07-03 04:14:01 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line with puts function
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
