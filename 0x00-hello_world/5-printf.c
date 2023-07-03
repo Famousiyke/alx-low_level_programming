be9d3c9f83cd58630f0561388889c4bfc8a0a3ce
famousiyke <rowlandiyke88@gmail.com>
2023-07-03 04:21:30 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line using the printf function
+ * followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
