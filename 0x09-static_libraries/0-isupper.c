6fafd2042f834183f75b8faa27bf5a01769169d3
famousiyke <rowlandiyke88@gmail.com>
2023-07-13 00:04:01 -0700

    create_static_lib.sh

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,11 +2,13 @@
 /**
  * _isupper - a program that checks for uppercase character
  * @lett: the chracter to be checked
- * Return: 1 if c is uppercase, 0 if not
+ * Return: 1 if c is uppercase, if not return 0
  */
 
 int _isupper(int lett)
 {
 	if (lett >= 'A' && lett <= 'Z')
 		return (1);
+	else
+	return (0);
 }
