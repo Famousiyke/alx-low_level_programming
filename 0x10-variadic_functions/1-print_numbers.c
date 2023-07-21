320da79dfa5ef1a52b1a5c72452765b0e665473d
Famousiyke <rowlandiyke88@gmail.com>
2023-07-20 21:53:55 -0700

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 /**
  *print_numbers - a function that prints numbers
- followed by a new line.
+ *followed by a new line.
  *@separator: string to be printed between numbers
  *@n: num of integers
  * Return: Always 0
